a8f1f54a383840fd1c04d838ef382084d00a7e5a
alibstill <alibstill@gmail.com>
2025-03-15 12:40:20 +0000

    [dezc-housing] - flesh out staging table

--- 02_analytics/housing/models/staging/stg_price_paid.sql
+++ 02_analytics/housing/models/staging/stg_price_paid.sql
@@ -1,7 +1,24 @@
-with price_paid as (
+with raw_price_paid as (
     select * from {{ source('land_registry', 'price_paid') }}
 )
 
 select 
-   *
-from price_paid
+   CAST(transaction_uid, AS STRING),
+   CAST(price AS INT64),
+   CAST(date_of_transfer AS DATE),
+   CAST(postcode AS STRING),
+   CAST(property_type AS STRING),
+   CAST(is_new_build AS STRING),
+   CAST(tenure_duration AS STRING),
+   CAST(paon AS STRING),
+   CAST(saon AS STRING),
+   CAST(street AS STRING),
+   CAST(locality AS STRING),
+   CAST(town_city AS STRING),
+   CAST(district AS STRING),
+   CAST(county AS STRING),
+   CAST(transaction_type AS STRING),
+   CAST(record_status AS STRING),
+   CAST(location_hash AS STRING)
+ 
+from raw_price_paid
