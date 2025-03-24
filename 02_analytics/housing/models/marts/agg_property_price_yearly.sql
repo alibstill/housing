660541f3586f899eeead3f8a95d1594125d96de3
alibstill <alibstill@gmail.com>
2025-03-24 16:11:11 +0000

    [dezc-housing] - fix mistake in agg_property_price_yearly (use ref not table names)

--- 02_analytics/housing/models/marts/agg_property_price_yearly.sql
+++ 02_analytics/housing/models/marts/agg_property_price_yearly.sql
@@ -1,3 +1,7 @@
+
+
+
+
 WITH aggregated_yearly_property_price_data AS (
     SELECT 
         dd.year, 
@@ -10,11 +14,11 @@ WITH aggregated_yearly_property_price_data AS (
         MIN(ps.price) min_price, 
         MAX(ps.price) max_price,
         APPROX_QUANTILES(ps.price, 100)[SAFE_OFFSET(50)] median_price
-    FROM `dezc-housing.housing.fact_property_sales` ps
-    INNER JOIN `dezc-housing.housing.dim_locations` ll ON ps.location_id = ll.location_id
-    INNER JOIN `dezc-housing.housing.dim_dates` dd on ps.date_of_transfer = dd.day_date
-    INNER JOIN `dezc-housing.housing.dim_property_types` pt on ps.property_type_id = pt.property_type_id
-    INNER JOIN `dezc-housing.housing.dim_tenures` t on ps.tenure_id = t.tenure_id
+    FROM {{ ref('fact_property_sales')}} ps
+    INNER JOIN {{ ref('dim_locations') }} ll ON ps.location_id = ll.location_id
+    INNER JOIN{{ ref('dim_dates')}} dd on ps.date_of_transfer = dd.day_date
+    INNER JOIN {{ ref('dim_property_types')}} pt on ps.property_type_id = pt.property_type_id
+    INNER JOIN {{ ref('dim_tenures')}} t on ps.tenure_id = t.tenure_id
     GROUP BY CUBE(dd.year, ll.county, pt.property_type_name, t.tenure_name, ps.is_new_build)
 )
 
