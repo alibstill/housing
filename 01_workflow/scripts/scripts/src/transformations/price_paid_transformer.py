7b5c8214916691f8bdcaa489435e7cb2d59f8ad8
alibstill <alibstill@gmail.com>
2025-08-05 18:51:38 +0100

    [dezc-housing] - add handle_types and clean functions to the price paid transformer

--- 01_workflow/scripts/scripts/src/transformations/price_paid_transformer.py
+++ 01_workflow/scripts/scripts/src/transformations/price_paid_transformer.py
@@ -25,7 +25,7 @@ def add_location_hash(dframe: pd.DataFrame, logger: Logger):
     ]
     # reorganize hash columns so that they appear like indexes. Note that if any of the
     # column values are NaN then they won't appear as a potential index. This approach
-    # allows us to handle any unwanted NaN columns
+    # allows us to make a hash from only the available address data
     df_stacked = dframe[hash_columns].stack()
 
     # Create a string from all the info in the location columns
@@ -37,10 +37,94 @@ def add_location_hash(dframe: pd.DataFrame, logger: Logger):
     df_copy = dframe.copy()
     df_copy["location_hash"] = location_hash
 
-    logger.info("Added 'location_hash' to price paid data.")
+    logger.info("Successfully added 'location_hash' to price paid data.")
     return df_copy
 
 
+def _check_log_drop(
+    message: str, filter, dframe: pd.DataFrame, logger: Logger
+) -> pd.DataFrame:
+    issue_rows = dframe[filter]
+    if len(issue_rows) > 0:
+        logger.info(
+            f"{message}. Number: %d. Transaction_uids = %s",
+            len(issue_rows),
+            list(issue_rows["transaction_uid"].values),
+        )
+        dframe = dframe.drop(issue_rows.index)
+    return dframe
+
+
+def clean(dframe: pd.DataFrame, logger: Logger):
+    """
+    Basic cleaning of data: drops any rows that have missing required fields
+
+    Parameters
+    ----------
+    dframe: pandas DataFrame
+        the dataframe of raw price paid data
+    logger: Logger
+    """
+    df_rows_no_transaction_ids = dframe[dframe["transaction_uid"].isna()]
+    if len(df_rows_no_transaction_ids) > 0:
+        logger.info(
+            "Rows with missing transaction_uid. Number: %d.",
+            len(df_rows_no_transaction_ids),
+        )
+        dframe = dframe.drop(df_rows_no_transaction_ids.index)
+
+    rows_missing_price_filter = dframe["price"].isna()
+    dframe = _check_log_drop(
+        "Rows with no 'price' data", rows_missing_price_filter, dframe, logger
+    )
+
+    rows_invalid_price_filter = dframe["price"] <= 0
+    dframe = _check_log_drop(
+        "Rows with 'price' set to value <= 0", rows_invalid_price_filter, dframe, logger
+    )
+
+    rows_invalid_date_of_transfer_filter = dframe["date_of_transfer"].isna()
+    dframe = _check_log_drop(
+        "Rows with 'date_of_transfer' not set",
+        rows_invalid_date_of_transfer_filter,
+        dframe,
+        logger,
+    )
+    logger.info("Successfully cleaned data.")
+    return dframe
+
+
+def handle_types(dframe: pd.DataFrame, logger: Logger):
+    """
+    Ensure all data has the expected types
+
+    Parameters
+    ----------
+    dframe: pandas DataFrame
+        the dataframe of raw price paid data
+    logger: Logger
+    """
+    dframe["date_of_transfer"] = pd.to_datetime(
+        dframe["date_of_transfer"], errors="coerce"
+    ).dt.date
+    logger.info("Successfully handled types")
+    return dframe
+
+
 class PricePaidTransformer:
+    def __init__(self) -> None:
+        self.transform_steps = [self.handle_types, self.clean, self.add_location_hash]
+
     def apply(self, dframe: pd.DataFrame, logger: Logger):
+        for step in self.transform_steps:
+            dframe = step(dframe, logger)
+        return dframe
+
+    def add_location_hash(self, dframe: pd.DataFrame, logger: Logger):
         return add_location_hash(dframe, logger)
+
+    def clean(self, dframe: pd.DataFrame, logger: Logger):
+        return clean(dframe, logger)
+
+    def handle_types(self, dframe: pd.DataFrame, logger: Logger):
+        return handle_types(dframe, logger)
--- 01_workflow/scripts/scripts/test/transformations/test_price_paid_transformer.py
+++ 01_workflow/scripts/scripts/test/transformations/test_price_paid_transformer.py
@@ -2,8 +2,35 @@ import pytest
 import pandas as pd
 import hashlib
 import logging
+from datetime import date
 
 from src.transformations import PricePaidTransformer
+from src.metadata import metadata_mapper
+
+
+def get_data(
+    transaction_uid: str = "{2ACACE8D-47C7-295E-E063-4804A8C0B0EB]",
+    price: int = 114000,
+    date_of_transfer: str = "2021-02-26 00:00:00",
+) -> dict[str, str]:
+    return {
+        "transaction_uid": f"{transaction_uid}",
+        "price": f"{price}",
+        "date_of_transfer": f"{date_of_transfer}",
+        "postcode": "S63 7FR",
+        "property_type": "S",
+        "is_new_build": "N",
+        "tenure_duration": "L",
+        "paon": "28",
+        "saon": "Flat 1",
+        "street": "REED WALK",
+        "locality": "WATH UPON DEARNE",
+        "town_city": "ROTHERHAM",
+        "district": "ROTHERHAM",
+        "county": "SOUTH YORKSHIRE",
+        "transaction_type": "A",
+        "record_status": "A",
+    }
 
 
 @pytest.fixture
@@ -27,7 +54,6 @@ def price_paid_dataframe():
         "record_status": ["A"],
     }
     df = pd.DataFrame(data)
-    df["data_of_transfer"] = pd.to_datetime(df["date_of_transfer"])
     return df
 
 
@@ -38,14 +64,37 @@ def test_logger():
 
 
 class TestPricePaidTransformer:
-    def test_should_add_location_hash_all_values_present(
+
+    def test_full_transformer(self, test_logger: logging.Logger) -> None:
+        valid_a = get_data(transaction_uid="1")
+        invalid_id = get_data()
+        invalid_id["transaction_uid"] = None
+        invalid_price = get_data(transaction_uid="2", price=0)
+        missing_date = get_data(transaction_uid="3", date_of_transfer=None)
+        valid_b = get_data(transaction_uid="4")
+        price_paid_meta = metadata_mapper.get("price_paid")
+        pp_dtypes = price_paid_meta.get_column_dtypes()
+
+        df = pd.DataFrame(
+            [valid_a, invalid_id, missing_date, invalid_price, valid_b]
+        ).astype(pp_dtypes)
+
+        result = PricePaidTransformer().apply(df, test_logger)
+
+        assert len(result) == 2
+        assert set(result["transaction_uid"].values) == {"1", "4"}
+        assert "location_hash" in result.columns
+
+    def test_add_location_hash_all_values_present(
         self, price_paid_dataframe: pd.DataFrame, test_logger: logging.Logger
     ) -> None:
         str_to_hash = "S63 7FR;28;Flat 1;REED WALK;WATH UPON DEARNE;ROTHERHAM;ROTHERHAM;SOUTH YORKSHIRE"
 
         expected_hash = hashlib.md5(str_to_hash.encode("utf-8")).hexdigest()
 
-        df_result = PricePaidTransformer().apply(price_paid_dataframe, test_logger)
+        df_result = PricePaidTransformer().add_location_hash(
+            price_paid_dataframe, test_logger
+        )
 
         assert "location_hash" in df_result.columns
 
@@ -88,7 +137,7 @@ class TestPricePaidTransformer:
             ),
         ],
     )
-    def test_should_add_location_hash_values_missing(
+    def test_add_location_hash_values_missing(
         self,
         price_paid_dataframe: pd.DataFrame,
         test_logger: logging.Logger,
@@ -100,8 +149,116 @@ class TestPricePaidTransformer:
 
         expected_hash = hashlib.md5(str_to_hash.encode("utf-8")).hexdigest()
 
-        df_result = PricePaidTransformer().apply(df_with_null, test_logger)
+        df_result = PricePaidTransformer().add_location_hash(df_with_null, test_logger)
 
         assert "location_hash" in df_result.columns
 
         assert df_result["location_hash"].iloc[0] == expected_hash
+
+    def test_handle_types_date_of_transfer_standard_format(
+        self,
+        price_paid_dataframe: pd.DataFrame,
+        test_logger: logging.Logger,
+    ) -> None:
+        assert len(price_paid_dataframe) == 1
+        assert isinstance(price_paid_dataframe["date_of_transfer"].iloc[0], str)
+
+        df_result = PricePaidTransformer().handle_types(
+            price_paid_dataframe, test_logger
+        )
+
+        assert isinstance(df_result["date_of_transfer"].iloc[0], date)
+
+    def test_handle_types_date_of_transfer_invalid_format(
+        self, test_logger: logging.Logger
+    ) -> None:
+        data = pd.DataFrame({"date_of_transfer": ["2023-01-01 00:00:00", "not_a_date"]})
+
+        df_result = PricePaidTransformer().handle_types(data, test_logger)
+
+        assert df_result["date_of_transfer"].isna().sum() == 1
+        first_valid_result = df_result.loc[
+            df_result["date_of_transfer"].notna(), "date_of_transfer"
+        ].iloc[0]
+        assert first_valid_result == date(2023, 1, 1)
+
+    def test_clean_logs_transaction_uids_rows_missing_price(
+        self,
+        caplog,
+        price_paid_dataframe: pd.DataFrame,
+        test_logger: logging.Logger,
+    ) -> None:
+        price_paid_dataframe["price"] = None
+
+        with caplog.at_level(logging.INFO):
+            PricePaidTransformer().clean(price_paid_dataframe, test_logger)
+
+        assert (
+            "Rows with no 'price' data. Number: 1. Transaction_uids = ['{2ACACE8D-47C7-295E-E063-4804A8C0B0EB]']"
+            in caplog.text
+        )
+
+    def test_clean_logs_transaction_uids_rows_invalid_price(
+        self,
+        caplog,
+        price_paid_dataframe: pd.DataFrame,
+        test_logger: logging.Logger,
+    ) -> None:
+        price_paid_dataframe["price"] = -1
+
+        with caplog.at_level(logging.INFO):
+            PricePaidTransformer().clean(price_paid_dataframe, test_logger)
+
+        assert (
+            "Rows with 'price' set to value <= 0. Number: 1. Transaction_uids = ['{2ACACE8D-47C7-295E-E063-4804A8C0B0EB]']"
+            in caplog.text
+        )
+
+    def test_clean_logs_transaction_uids_rows_missing_date_of_transfer(
+        self,
+        caplog,
+        price_paid_dataframe: pd.DataFrame,
+        test_logger: logging.Logger,
+    ) -> None:
+        price_paid_dataframe["date_of_transfer"] = None
+
+        with caplog.at_level(logging.INFO):
+            PricePaidTransformer().clean(price_paid_dataframe, test_logger)
+
+        assert (
+            "Rows with 'date_of_transfer' not set. Number: 1. Transaction_uids = ['{2ACACE8D-47C7-295E-E063-4804A8C0B0EB]']"
+            in caplog.text
+        )
+
+    def test_clean_logs_missing_transaction_uids(
+        self,
+        caplog,
+        price_paid_dataframe: pd.DataFrame,
+        test_logger: logging.Logger,
+    ) -> None:
+        price_paid_dataframe["transaction_uid"] = None
+
+        with caplog.at_level(logging.INFO):
+            PricePaidTransformer().clean(price_paid_dataframe, test_logger)
+
+        assert "Rows with missing transaction_uid. Number: 1." in caplog.text
+
+    def test_clean_drops_rows_where_required_field_invalid(
+        self, test_logger: logging.Logger
+    ) -> None:
+        data = pd.DataFrame(
+            {
+                "price": [100, 200, -1, None, 500],
+                "date_of_transfer": [
+                    date(2025, 3, 3),
+                    date(2025, 3, 3),
+                    date(2025, 3, 4),
+                    date(2025, 3, 5),
+                    pd.NaT,  # missing date
+                ],
+                "transaction_uid": [None, "1", "2", "3", "4"],
+            }
+        )
+        df_result = PricePaidTransformer().clean(data, test_logger)
+
+        assert len(df_result) == 1
